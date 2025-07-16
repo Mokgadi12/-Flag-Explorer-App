03b109f57e15611416cedbc50f871e772df8a712
R8872826 <R8872826@rmb.co.za>
2025-07-16 19:07:13 +0200

    Backend project

---
+++ Flag App/Flag-App/src/main/java/com/example/Flag/App/FlagAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Flag.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlagAppApplication.class, args);
+	}
+
+}
---
+++ Flag App/Flag-App/src/test/java/com/example/Flag/App/FlagAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Flag.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlagAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
